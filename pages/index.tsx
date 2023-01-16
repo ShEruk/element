5a3cbf1fcdf35a4b7322e1ff1962a736e0377e98
ShEruk <sh_murakami@feel-style.jp>
2023-01-16 10:40:21 +0900

    20230116

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,85 +1,48 @@
 import type { NextPage } from 'next'
+import Router from 'next/router'
 import Head from 'next/head'
 import Image from 'next/image'
+import Box from '@mui/material/Box'
+import Paper from '@mui/material/Paper'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
 
 const Home: NextPage = () => {
+  const handleButtonClick = () => {
+    Router.push('/main')
+  }
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{"Element"}</title>
+        <meta name="description" content="スタートページ"/>
       </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="flex justify-center alingn-center w-auto h-screen m-auto bg-gray-200">
+          <Paper className="container-lg flex flex-col w-2/3 h-2/3 m-auto bg-white ">
+            <Box className="w-2/3 m-auto text-center bg-gray-100">
+              <Typography variant={"h5"}>
+                はじめに
+              </Typography>
+              <Box className="m-3">
+                <Typography>
+                  プログラミング
+                  プログラミングとは、「コンピュータにさせたい仕事を順番に書き出す作業」のこと。
+                  コンピューターが理解できる言語で命令を出さなければいけません。
+                  
+                  プログラム
+                  プログラムとは、「コンピュータにやってほしいことを理解できる言葉にしたもの」。
+                  
+                  プログラミング言語
+                  「コンピュータが理解できる言葉＝プログラミング言語」であり、コンピュータに命令できる言語
+                </Typography>
+              </Box>
+            </Box>
+            <Button className="bg-blue-500 hover:bg-blue-600 w-40 h-14 mt-2 mb-2 m-auto" variant={"contained"} onClick={handleButtonClick}>
+              {"Get Ready"}
+            </Button>
+          </Paper>
         </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
+    </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,25 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    fontFamily: {
+      body: [
+        'Avenir',
+        'Helvetica Neue',
+        'Helvetica',
+        'Arial',
+        'Hiragino Sans',
+        'ヒラギノ角ゴシック',
+        'メイリオ',
+        'Meiryo',
+        'YuGothic',
+        'Yu Gothic',
+        'ＭＳ Ｐゴシック',
+        'MS PGothic',
+        'sans-serif'
+      ]
+    },
     extend: {},
   },
   plugins: [],
+  important: true,
 }
